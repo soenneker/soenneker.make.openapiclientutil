1082ef3cb0fc2c22bd441dd7556e561f59d6c7f5
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:03 -0500

    xml commenting

--- src/Soenneker.Make.OpenApiClientUtil/Abstract/IMakeOpenApiClientUtil.cs
+++ src/Soenneker.Make.OpenApiClientUtil/Abstract/IMakeOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Make.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IMakeOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<MakeOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
